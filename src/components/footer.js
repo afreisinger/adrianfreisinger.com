72f714a2d65a4ed6a88d2780e8f38a52c5653698
Adrian Freisinger <afreisinger@gmail.com>
2026-02-13 23:14:07 -0300

    💄 style(footer): cleanup code and add env support for github stats

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
 import { socialMedia, name } from '@config';
+import PropTypes from 'prop-types';
 
 const StyledFooter = styled.footer`
   ${({ theme }) => theme.mixins.flexCenter};
@@ -74,31 +75,67 @@ const StyledCredit = styled.div`
   }
 `;
 
-const Footer = () => (
-  <StyledFooter>
-    <StyledSocialLinks>
-      <ul>
-        {socialMedia &&
-          socialMedia.map(({ name, url }, i) => (
-            <li key={i}>
-              <a href={url} aria-label={name}>
-                <Icon name={name} />
-              </a>
-            </li>
-          ))}
-      </ul>
-    </StyledSocialLinks>
+const Footer = () => {
+  const [githubInfo, setGitHubInfo] = useState({
+    stars: null,
+    forks: null,
+  });
 
-    <StyledCredit tabindex="-1">
-      <div className="copy">
-        <span style={{ fontSize: '1rem', marginRight: '5px' }}>&copy;</span> {name}{' '}
-        <span style={{ fontSize: '.5rem', color: 'var(--light-gray)', margin: '0px 5px' }}>
-          &bull;
-        </span>{' '}
-        2026
-      </div>
-    </StyledCredit>
-  </StyledFooter>
-);
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'production' && !process.env.GATSBY_FETCH_STATS) {
+      return;
+    }
+    fetch('https://api.github.com/repos/afreisinger/adrianfreisinger.com')
+      .then(response => response.json())
+      .then(json => {
+        const { stargazers_count, forks_count } = json;
+        setGitHubInfo({
+          stars: stargazers_count,
+          forks: forks_count,
+        });
+      })
+      .catch(e => console.error(e));
+  }, []);
+
+  return (
+    <StyledFooter>
+      <StyledSocialLinks>
+        <ul>
+          {socialMedia &&
+            socialMedia.map(({ name, url }, i) => (
+              <li key={i}>
+                <a href={url} aria-label={name}>
+                  <Icon name={name} />
+                </a>
+              </li>
+            ))}
+        </ul>
+      </StyledSocialLinks>
+
+      <StyledCredit tabindex="-1">
+        <a href="https://github.com/afreisinger/adrianfreisinger.com">
+          <div>Designed &amp; Built by Adrián Freisinger</div>
+
+          {githubInfo.stars && githubInfo.forks && (
+            <div className="github-stats">
+              <span>
+                <Icon name="Star" />
+                <span>{githubInfo.stars.toLocaleString()}</span>
+              </span>
+              <span>
+                <Icon name="Fork" />
+                <span>{githubInfo.forks.toLocaleString()}</span>
+              </span>
+            </div>
+          )}
+        </a>
+      </StyledCredit>
+    </StyledFooter>
+  );
+};
+
+Footer.propTypes = {
+  githubInfo: PropTypes.object,
+};
 
 export default Footer;
